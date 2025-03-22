54d99d0e9385464eb243868ceee946b47184b7be
Arnav Saini <arnavsaini114@gmail.com>
2025-03-22 07:21:23 +0530

    fixes

--- api/scrape.js
+++ api/scrape.js
@@ -2,6 +2,11 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 module.exports = async (req, res) => {
+  // Allow CORS for local development
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
